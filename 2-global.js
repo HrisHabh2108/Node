55dd1f2d3197ff2fa0fe8cf5cb987566e5b5a9f8
HrisHabh2108 <hrishabh.deshmukh.ece22@itbhu.ac.in>
2024-12-28 09:26:17 +0530

    first commit

---
+++ 2-global.js
@@ -0,0 +1,11 @@
+//Global-No window
+//__dirname-path of current directory
+//__filename-filename
+//require-function to use modules
+//module-info about current module
+//process-info about env where the program is being executed
+
+console.log(__dirname)
+setInterval(()=>{
+    console.log('hello world')
+},1000)
---
+++ 4-names.js
@@ -0,0 +1,6 @@
+//local
+const secret='SUPER SECRET'
+//share
+const hrish='hrishabh'
+const dish='deshmukh'
+module.exports={hrish,dish}
